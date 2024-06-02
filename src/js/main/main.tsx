e45c4d831b99ea44b1959116fceebff647d4ce0d
unknown <adam.vaizman@gmail.com>
2024-06-02 14:41:58 +0300

    init

---
+++ src/js/main/main.tsx
@@ -0,0 +1,18 @@
+function addSubstitles() {
+  
+} 
+
+
+
+const Main = () => {
+  // const app = 
+  // alert(JSON.stringify(window.app))
+
+  return (
+    <div>
+      <h1 style={{ color: "blue" }}>WONDERBOLT</h1>
+      <button onClick={addSubstitles}>ADD CAPS</button>
+    </div>
+  );
+};
+export default Main;
